32b7d55d54621a20d4f734c91168cb8f40727130
TheusMartins <ml_martins@yahoo.com>
2021-05-01 16:23:16 -0300

    Add last section into settings view

--- Fructus/App/Settings/SettingsView.swift
+++ Fructus/App/Settings/SettingsView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct SettingsView: View {
     // MARK: - Properties
     @Environment(\.presentationMode) private var presentationMode
+    @AppStorage("isOnOnboarding") var isOnOnboarding: Bool = false
     
     // MARK: - View
     var body: some View {
@@ -32,6 +33,35 @@ struct SettingsView: View {
                             Text("Most fruits are naturally low in fat, sodium, and calories. Nonemhave cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary, fiber, vitamins, and much more")
                                 .font(.footnote)
                         }
+                    }
+                    
+                    GroupBox(
+                        label: SectionLabelView(titleText: "Customization", imageString: "paintbrush")) {
+                        Divider().padding(.vertical, 4)
+                        
+                        Text("If you wish, you can restart the application by toggle the switch in this box. That way it starts the onboarding process and will see the welcome screen again.")
+                            .padding(.vertical, 8)
+                            .frame(minHeight: 60)
+                            .layoutPriority(1)
+                            .font(.footnote)
+                            .multilineTextAlignment(.leading)
+                        
+                        Toggle(isOn: $isOnOnboarding) {
+                            if isOnOnboarding {
+                                Text("Restarted".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.green)
+                            } else {
+                                Text("Restart".uppercased())
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                            }
+                        }
+                        .padding()
+                        .background(
+                            Color(UIColor.tertiarySystemBackground)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .continuous))
+                        )
                     }
                     
                     GroupBox(
